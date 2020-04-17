c8c985c4f12bb422fa4fa0cc40ce59f6e3de3ea7
saramortara <saramortara@gmail.com>
2020-04-17 17:37:14 -0300

    abbreviated genus + another fix in remove_authors

--- R/check_string.R
+++ R/check_string.R
@@ -104,10 +104,20 @@ check_string <- function(scientificName = NULL){
   # definindo prevalencia
   prev <- c("affinis", "conferre", "subspecies", "variety", "indet", "forma", "incertae sedis")
 
-  #2. recognizig authors ####
+  #2. sp. nov.####
+  #sp. nov., spec. nov., sp. n., nov. sp., nov. spec. or n. sp.
+  spnov_regex <- "\\ssp\\.\\snov\\.|\\sspec\\.\\snov\\.|\\ssp\\.\\sn\\.|\\snov\\.\\ssp\\.
+  |\\snov\\.\\sspec\\.|\\sn\\.\\sp\\."
+  spnov <- stringr::str_detect(check$scientificName,
+                               stringr::regex(spnov_regex,
+                                              ignore_case = TRUE))
+  check$scientificName_status[spnov] <- "species_nova"
+  check$scientificName_new[spnov] <- scientificName[spnov]
+
+  #3. recognizig authors ####
   no_authors <- sapply(check$scientificName_new, flora::remove.authors)
   # aqui aff cf subsp var e indet prevalescem
-  id_authors <- is.na(check$scientificName_status) & check$scientificName_new != no_authors |
+  id_authors <- is.na(check$scientificName_status) | #& check$scientificName_new != no_authors |
     sapply(strsplit(as.character(check$scientificName), " "), length) > 2 &
     !check$scientificName_status %in% prev
   # removing f. in the end of author name
@@ -132,16 +142,6 @@ check_string <- function(scientificName = NULL){
   id_digits <- stringr::str_detect(check$scientificName, '\\d') &
     !check$scientificName_status %in% c(prev, "name_w_authors")
   check$scientificName_status[id_digits] <- "not_name_has_digits"
-
-  #5. sp. nov.####
-  #sp. nov., spec. nov., sp. n., nov. sp., nov. spec. or n. sp.
-  spnov_regex <- "\\ssp\\.\\snov\\.|\\sspec\\.\\snov\\.|\\ssp\\.\\sn\\.|\\snov\\.\\ssp\\.
-  |\\snov\\.\\sspec\\.|\\sn\\.\\sp\\."
-  spnov <- stringr::str_detect(check$scientificName,
-                               stringr::regex(spnov_regex,
-                                              ignore_case = TRUE))
-  check$scientificName_status[spnov] <- "species_nova"
-  check$scientificName_new[spnov] <- scientificName[spnov]
 
   #6. names not matching Genus + species pattern ####
   # de novo incluir prevalencia
@@ -173,6 +173,13 @@ check_string <- function(scientificName = NULL){
   #10. hybrid ####
   hybrid <- str_detect(check$scientificName_new, "×")
   check$scientificName_status[hybrid] <- "hybrid_species"
+
+  # 11 abreviated genus ####
+  genus <- sapply(str_split(check$scientificName_new, " "), function(x) x[1])
+  abbrev_gen <- gsub("\\.", "", genus)
+  char_gen <- nchar(abbrev_gen)
+  abbrev_gen <- char_gen == 1
+  check$scientificName_status[abbrev_gen] <- "abbreviated_genus"
 
   #11. possibly ok ####
   check$scientificName_status[is.na(check$scientificName_status)] <- "possibly_ok"
