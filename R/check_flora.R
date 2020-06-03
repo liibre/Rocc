93803b7721f971de384749bb8587be403f343534
saramortara <saramortara@gmail.com>
2020-06-03 19:56:08 -0300

    fix bug in synonyms_base indexation

--- R/check_flora.R
+++ R/check_flora.R
@@ -92,7 +92,7 @@ check_flora <- function(species,
         synonyms <- synonyms[, !names(synonyms) %in% syn_remove]
         synonyms_base <- out[out$taxonomicStatus %in% "NOME_ACEITO"
                              & is.na(out$infraspecificEpithet), ]
-        synonyms_base <- out[, c('taxonID', 'species')]
+        synonyms_base <- synonyms_base[, c('taxonID', 'species')]
         names(synonyms_base) <- c("taxonID_base", "species_base")
         # juntando a info do basinomio com o output de synonyms
         synonyms <- cbind(synonyms, synonyms_base)
