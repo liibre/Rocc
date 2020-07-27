babd56d9da3c19e0a40361a5ad0f432135e9965b
saramortara <saramortara@gmail.com>
2020-07-27 16:55:57 -0300

    function to add infraspecies identification if missing, still don't know if it's a keeper or where it should be

---
+++ R/set_infraspecies.R
@@ -0,0 +1,22 @@
+#' Adds infraspecies identification if missing
+#'
+#' @param subspecies Species name containing three stings: Genus, species epithet, and infraspecies epithet
+#' @param type Type of infraspecies classification. Any type in: `"var"` or `"subsp"` or `"form"`.
+#'
+#' @export
+#' @examples
+#' set_infraspecies("Lindsaea lancea lancea", type = "var")
+#'
+set_infraspecies <- function(subspecies,
+                              type) {
+  types <- data.frame(var = "var.",
+                      subsp = "subsp.",
+                      form = "form.")
+  if (!type %in% names(types)) {
+    stop("type must be either 'var', 'subsp' or 'form'")
+  }
+  infra_cat <- types[names(types) %in% type]
+  infrasp <- sapply(strsplit(subspecies, " "), function(x) {paste(x[1], x[2], infra_cat, x[3])})
+  return(data.frame(verbatimSpecies = subspecies,
+                    subspecies = infrasp))
+}
